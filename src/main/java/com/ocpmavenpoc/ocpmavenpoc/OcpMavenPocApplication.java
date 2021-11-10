db33416fb8634d9eca9b32db0a3f34154551912e
yucreat101 <lavikumar@lavis-MBP-2.fios-router.home>
2021-11-09 21:48:33 -0500

    first commit

---
+++ src/main/java/com/ocpmavenpoc/ocpmavenpoc/OcpMavenPocApplication.java
@@ -0,0 +1,13 @@
+package com.ocpmavenpoc.ocpmavenpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcpMavenPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcpMavenPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ocpmavenpoc/ocpmavenpoc/OcpMavenPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocpmavenpoc.ocpmavenpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcpMavenPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
